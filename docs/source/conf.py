dbea03ac7ab64f97efdaa4cbb2e49259d189ae08
Abdel-Rahman Ibrahim <aibrahimv2@gmail.com>
2021-10-08 08:49:32 -0400

    Added 'sphinx.ext.napoleon' to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,8 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinx_rtd_theme'
+    'sphinx_rtd_theme',
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
